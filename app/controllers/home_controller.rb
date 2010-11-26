bee2bf5f6e14d10e25f2d17a2c1694a4f44acd02
Rafael Matuoka <rmatuoka@agenciaeconomica.com.br>
2010-11-26 12:44:43 -0200

    Ajuste noticias

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,5 @@
 class HomeController < ApplicationController
+  @NoticiaDest =  Notice.find(:first, :conditions => ['published = 1'])
+  
+  @Noticias = Notice.find(:all, :conditions => ['published = 1'], :limit => "1,4")
 end
