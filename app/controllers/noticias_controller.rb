b2ba5c9b0527051027911c03b5e896b3fc4ca991
Rafael Matuoka <rmatuoka@agenciaeconomica.com.br>
2010-11-29 15:33:53 -0200

    Ajustes video e todas noticiasgit add .

--- app/controllers/noticias_controller.rb
+++ app/controllers/noticias_controller.rb
@@ -1,4 +1,8 @@
 class NoticiasController < ApplicationController
+  def index
+    @Noticias = Notice.all(:conditions => "published = 1")  
+  end
+  
   def show
     @Noticia = Notice.find(params[:id])
   end
